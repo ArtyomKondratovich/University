06df8a49508a86b67676cd7f22c317d51f1cc4a9
ArtyomKondratovich <kondratovich443@gmail.com>
2024-03-11 14:19:32 +0300

    added report 2

--- lab_1_1/lab_1_/HelpClasses/Function.cs
+++ lab_1_1/lab_1_/HelpClasses/Function.cs
@@ -2,8 +2,8 @@
 {
     public class Function
     {
-        public Func<double, double> F1 = (x) => Math.Sin(x) * Math.Cos(x);
-        public  Func<double, double> F2 = (x) => 1 / (1 + 12 * Math.Pow(x, 4));
+        public Func<double, double> F1 = (x) => Math.Sin(Math.Cos(x));
+        public  Func<double, double> F2 = (x) => Math.Abs(Math.Abs(x) - 1);
         public MyRange Range;
 
         public Function(MyRange range)
